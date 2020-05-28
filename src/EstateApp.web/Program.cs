08be829bcf0b6162ddcbd8e83d7ade1fde6706db
Oladiti <oladiti_samuel24@yahoo.com>
2020-05-28 10:37:13 +0100

    added data

--- src/EstateApp.web/Program.cs
+++ src/EstateApp.web/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace EstateApp
+namespace EstateApp.web
 {
     public class Program
     {
