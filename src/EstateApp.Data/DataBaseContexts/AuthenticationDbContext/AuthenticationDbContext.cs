09fdafbb33758488a76fa86c9879cb649eeb5e96
Oladiti <oladiti_samuel24@yahoo.com>
2020-05-29 02:18:21 +0100

    added authentication db context class

--- src/EstateApp.Data/DataBaseContexts/AuthenticationDbContext/AuthenticationDbContext.cs
+++ src/EstateApp.Data/DataBaseContexts/AuthenticationDbContext/AuthenticationDbContext.cs
@@ -1,7 +1,16 @@
+using System.Net.Mime;
+using EsatateApp.Data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
 namespace EsatateApp.Data.DatabaseContexts.AuthenticationDbContext
 {
-    public class AuthenticationDbContext : IdentityDbContext
+    public class AuthenticationDbContext : IdentityDbContext<ApplicationUser>
     {
-        
+        public AuthenticationDbContext(DbContextOptions<AuthenticationDbContext> options)
+            : base(options)
+            {
+                
+            }
     }
 }
