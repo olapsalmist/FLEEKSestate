10ef8c256e2932998b8e0a8a58df8bf24d88e917
Oladiti <oladiti_samuel24@yahoo.com>
2020-06-07 12:23:24 +0100

    added migrations

---
+++ src/EstateApp.Data/Migrations/ApplicationDb/20200607111125_initial Application Migration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EstateApp.Data.Migrations.ApplicationDb
+{
+    public partial class initialApplicationMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
